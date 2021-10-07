e9f6e41bcc795b35fc1f554c021203dfcbfd4b6b
neely <34224624+neelybd@users.noreply.github.com>
2021-10-06 20:01:44 -0700

    1.1.1    - Added Print Statements

--- BN_CSV_Union.py
+++ BN_CSV_Union.py
@@ -4,8 +4,8 @@ import sys
 
 def main():
     print("Program: BN_CSV_Union")
-    print("Release: 1.1.0")
-    print("Date: 2019-12-11")
+    print("Release: 1.1.1")
+    print("Date: 2021-10-06")
     print("Author: Brian Neely")
     print()
     print()
@@ -25,12 +25,18 @@ def main():
     # Create a blank variable
     concatenated = None
 
+    # Ask for output
+    file_out_path = select_file_out_csv(files_in[0])
+
     # Concatenate variables
     for i in files_in:
+        print('Opening File: ' + i)
         concatenated = pd.concat([concatenated, open_unknown_csv(i, delimination)])
+        print('File: ' + i + ' opened!')
+        print()
 
     # Export file
-    concatenated.to_csv(select_file_out_csv(files_in[0]), index=False)
+    concatenated.to_csv(file_out_path, index=False)
 
     # CSV's concatenated
     input("CSV's concatenated! Press enter to close...")
